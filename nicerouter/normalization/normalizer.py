5061a46dda8fb5aa6bfbceedcbafacce8fe6608a
Fabian Gruenwald <fabian.gruenwald@mni.thm.de>
2025-12-05 15:36:25 +0100

    heavily improved performance of normalizer

--- nicerouter/normalization/normalizer.py
+++ nicerouter/normalization/normalizer.py
@@ -3,7 +3,7 @@ from typing import Any, Callable, Sequence, get_origin
 from pydantic import BaseModel 
 from nicerouter.type_utils import extract_most_inner_type
 from nicerouter.normalization.type_util import is_list_reference_type, is_reference_type
-
+from functools import cache
 type IdResolver = Callable[[BaseModel, str], str]
 
 class ObjectNormalizer:
@@ -15,46 +15,79 @@ class ObjectNormalizer:
          
          self.ref2idfield_resolver = ref2idfield_resolver
          self.store = defaultdict(dict)
-        
+    
+    @cache
+    def _is_reference_type(self, x):
+         return is_reference_type(x)
+    
+    @cache
+    def _is_list_reference_type(self, x):
+         return is_list_reference_type(x)
+    
+    @cache
+    def _extract_most_inner_type(self, model: type[BaseModel], field_name: str):
+         field_info = model.model_fields.get(field_name)
+         if field_info is None:
+              raise ValueError(f"{model=}:{field_info=}. field_info was None. Not sure how to handle this.")
+         annotation = field_info.annotation
+         if annotation is None:
+              raise ValueError(f"{model=}:{field_info=}. annotation was None. Not sure how to handle this.")
+         return extract_most_inner_type(annotation)
+
+    
+    @cache
+    def _get_model_metadata(self, model_type: type[BaseModel]):
+
+        model_fields = model_type.model_fields
+
+        reference_fields = {
+            field_name: field_info for 
+            field_name, field_info in model_fields.items()
+            if self._is_reference_type(field_info.annotation) # type: ignore
+        }
+
+        list_fields = {
+            field_name: field_info for 
+            field_name, field_info in model_fields.items()
+            if self._is_list_reference_type(field_info.annotation) # type: ignore
+        }
+
+        # 3. Identify Standard Fields (Scalars) to copy directly
+        # We exclude refs and lists so we don't dump them unnecessarily
+        standard = set(model_fields.keys()) - set(reference_fields.keys()) - set(list_fields.keys())
+
+        return reference_fields, list_fields, standard
+    
     def normalize(
             self,
             *,
-            obj: BaseModel,
+            obj: Any,
+            obj_model: type[BaseModel],
             id_name: str = "id",
             ref2idfield_resolver: IdResolver | None = None 
         ) -> dict[str, Any]:
             
             ref2idfield_resolver = ref2idfield_resolver or self.ref2idfield_resolver
             # get or create slice for this type
-            store_slice = self.store[obj.__class__.__name__]
+            slicename = obj_model.__name__
+            store_slice = self.store[slicename]
 
             # return early if its already normalized
             id = getattr(obj, id_name)
             if id in store_slice:
                  return store_slice[id]
             
-            model_fields = type(obj).model_fields
+            reference_fields, list_fields, standard_fields = self._get_model_metadata(obj_model)
+            
+            # normalized_obj = obj.model_dump(mode="python", exclude_none=False)
+            normalized_obj = {k: getattr(obj, k) for k in standard_fields}
 
-            normalized_obj = obj.model_dump(mode="python", exclude_none=False)
-
-            reference_fields = {
-                field_name: field_info for 
-                field_name, field_info in model_fields.items()
-                if is_reference_type(field_info.annotation) # type: ignore
-            }
-
-            list_fields = {
-                field_name: field_info for 
-                field_name, field_info in model_fields.items()
-                if is_list_reference_type(field_info.annotation) # type: ignore
-            }
- 
+            # normalize ref children
             for field_name in reference_fields:
                 
                 ref_field_obj = getattr(obj, field_name)
 
-                if ref_field_obj is None:
-                    # setattr(normalized_obj, field_name, None)
+                if ref_field_obj is None: 
                     normalized_obj[field_name] = None
                     continue
                 
@@ -67,18 +100,23 @@ class ObjectNormalizer:
                           raise ValueError(msg)
                     id_field = ref2idfield_resolver(obj, field_name)
 
-                self.normalize(obj=ref_field_obj, id_name=id_name)
-                normalized_obj[id_field] = normalized_obj[field_name][id_name]
-                del normalized_obj[field_name]
+                # normalize child
+                ref_field_obj_model = self._extract_most_inner_type(obj_model, field_name)
+                 
+                normalized_child = self.normalize(obj=ref_field_obj, obj_model=ref_field_obj_model, id_name=id_name)
+                # set id field with id of child
+                normalized_obj[id_field] = normalized_child[id_name] 
 
+            # normalize ref children inside list
             for field_name in list_fields: 
- 
+
+                ref_field_obj_model = self._extract_most_inner_type(obj_model, field_name) 
                 # normalize and set list of ids field as part of normalized entity
                 normalized_obj[field_name] = [
-                    self.normalize(obj=o)[id_name] for o in getattr(obj, field_name)
+                    self.normalize(obj=o, obj_model=ref_field_obj_model)[id_name] for o in getattr(obj, field_name)
                 ]
 
-            # store normalized object in slice
+            # store normalized object in slice 
             store_slice[id] = normalized_obj
 
             return normalized_obj
